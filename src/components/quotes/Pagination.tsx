cb6f9bbd64512874c7a4cea402264c0da1c5d0ba
Marco Civello <marco.civello@gmail.com>
2024-06-21 09:19:02 +0200

    fix: fixed Pagination css

--- src/components/quotes/Pagination.tsx
+++ src/components/quotes/Pagination.tsx
@@ -1,24 +1,30 @@
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
 interface PaginationProps {
   currentPage: number;
   paginationNumbers: number[];
   onPageChange: (page: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, paginationNumbers, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  paginationNumbers,
+  onPageChange,
+}) => {
   return (
     <div className="mt-6 flex flex-wrap justify-center space-x-2">
       <button
         onClick={() => onPageChange(Math.max(currentPage - 1, 1))}
         disabled={currentPage === 1}
-        className="btn-outline mt-2"
+        className="btn-outline mt-2 h-10 w-auto"
       >
-        Previous
+        <FaChevronLeft className="h-3 w-3" />
       </button>
       {paginationNumbers.map((pageNumber: number) => (
         <button
           key={pageNumber}
           onClick={() => onPageChange(pageNumber)}
-          className={`btn-outline mt-2 w-10 ${
+          className={`btn-outline mt-2 h-10 w-10 ${
             currentPage === pageNumber ? "!border-cyan-600 !bg-cyan-600 !text-white" : ""
           }`}
         >
@@ -28,9 +34,9 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, paginationNumbers,
       <button
         onClick={() => onPageChange(Math.min(currentPage + 1, paginationNumbers.length))}
         disabled={currentPage === paginationNumbers.length}
-        className="btn-outline mt-2"
+        className="btn-outline mt-2 h-10 w-auto"
       >
-        Next
+        <FaChevronRight className="h-3 w-3" />
       </button>
     </div>
   );
